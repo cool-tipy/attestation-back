da5db138d773b556b61c4be3f7a6e7906f131e15
Булат <hatmullinbulat24@gmail.com>
2025-07-11 18:39:59 +0500

    исправил ошибку импорта

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import cookie from "@elysiajs/cookie";
+import { cookie } from "@elysiajs/cookie";
 import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { authHandler } from "./modules/auth/auth";
@@ -6,16 +6,18 @@ import cors from "@elysiajs/cors";
 
 const app = new Elysia()
   .use(swagger())
-  .use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:8080'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  }))
+  .use(
+    cors({
+      origin: ["http://localhost:5173", "http://localhost:8080"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  )
   .use(cookie())
-  .get('/', () => 'Welcome!!!')
+  .get("/", () => "Welcome!!!")
   .use(authHandler)
-  .listen(3000)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
