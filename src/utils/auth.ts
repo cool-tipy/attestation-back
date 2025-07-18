1d73dd2d928f0648d6ff7e2e3cf347850d466d30
Ившин Виталий <lumberpants471@gmail.com>
2025-07-18 12:13:35 +0500

    тест 5

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -9,15 +9,13 @@ export function verifyToken(headers: Record<string, string | undefined>, set: an
 
   const token = headers['authorization']
   if (!token) {
-    set.status = 401
-    throw new Error('Token not provided')
+    return
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!)
     return decoded
   } catch (err) {
-    set.status = 401
-    throw new Error('Invalid token')
+    return
   }
 }
